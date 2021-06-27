8e9edc8ed8894d54347c6242f2982ae25d15b441
Vasileios Kolias <v.kolias@gmail.com>
2021-06-27 18:14:22 +0300

    third

--- prj3.js
+++ prj3.js
@@ -1,8 +1,8 @@
 fetch('http://10.0.2.4:16986/?token=1')
 .then(response => {
     if (!response.ok) {
-        fetch('http://10.0.2.4:16986/?token=cccNOK');
+        fetch('http://10.0.2.4:16986/?token=NOK');
     }else{
-        fetch('http://10.0.2.4:16986/?token=cccOK');
+        fetch('http://10.0.2.4:16986/?token=OK');
     }
 });
